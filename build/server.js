4d7de57df88bc327d67ac0baff86ce0c4a39d6d6
Robin Wenglewski <robin@wenglewski.de>
2013-05-21 13:03:30 +0200

    make docs work again

--- build/server.js
+++ build/server.js
@@ -1,15 +1,26 @@
 (function() {
-  var file, nStatic;
+  var docs, example, nStatic;
 
   nStatic = require('node-static');
 
-  file = new nStatic.Server('build/example');
+  example = new nStatic.Server('build/example');
+
+  docs = new nStatic.Server('doc');
 
   require('http').createServer(function(request, response) {
-    console.log("Listening ...");
     return request.addListener('end', function() {
-      return file.serve(request, response);
+      var newUrl;
+
+      if (newUrl = request.url.replace(/^\/docs/, '')) {
+        console.log("serving docs");
+        request.url = newUrl;
+        return docs.serve(request, response);
+      } else {
+        return example.serve(request, response);
+      }
     }).resume();
   }).listen(process.env.PORT || 5000);
+
+  console.log("Listening ...");
 
 }).call(this);
--- src/server.coffee
+++ src/server.coffee
@@ -1,10 +1,16 @@
 nStatic = require 'node-static'
 
-file = new(nStatic.Server)('build/example')
+example = new(nStatic.Server)('build/example')
+docs = new(nStatic.Server)('doc')
 
 require('http').createServer (request, response) ->
-	console.log "Listening ..."
 	request.addListener 'end', ->
-		file.serve request, response
+		if newUrl = request.url.replace /^\/docs/, ''
+			console.log "serving docs"
+			request.url = newUrl
+			docs.serve request, response
+		else
+			example.serve request, response
 	.resume()
 .listen(process.env.PORT || 5000)
+console.log "Listening ..."
